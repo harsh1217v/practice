e8d1006d97927947ea860a7cd6452558f1ca9b72
harsh69 <harshdongare17@gmail.com>
2026-02-23 12:29:47 +0530

    Add files via upload

---
+++ lsit_HW.py
@@ -0,0 +1,14 @@
+rcb = ["Virat Kohli"," Phil Salt"," Devdutt Padikkal"," Rajat Patidar"," Liam Livingstone", "Jitesh Sharma", "Tim David"," Krunal Pandya", "Bhuvneshwar Kumar", "Josh Hazlewood" ]
+
+mi = ["Rohit Sharma", "Ryan Rickelton", "Suryakumar Yadav", "Tilak Varma", "Hardik Pandya", "Will Jacks", "Naman Dhir", "Mitchell Santner", "Jasprit Bumrah", "Trent Boult"]
+
+pbsk = ["Prabhsimran Singh", "Priyansh Arya", "Shreyas Iyer", "Shashank Singh", "Marcus Stoinis", "Glenn Maxwell", "Suryansh Shedge", "Azmatullah Omarzai", "Marco Jansen", "Arshdeep Singh"]
+
+gt = ["Shubman Gill", "Jos Buttler", "Sai Sudharsan", "Shahrukh Khan", "Rahul Tewatia", "Sai Kishore", "Arshad Khan", "Rashid Khan", "Kagiso Rabada", "Mohammed Siraj"]
+
+rcb.append("yash dayal")
+mi.append("Karn Sharma")
+pbsk.append("Yuzvendra Chahal")
+gt.append("Prasidh Krishna")
+
+print(rcb,mi,gt,pbsk )
